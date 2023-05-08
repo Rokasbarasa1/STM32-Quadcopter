e63e3f86c71d4cd14685d837d2a8f7c0fe0ab03b
Rokas <50622827+Rokasbarasa1@users.noreply.github.com>
2023-05-08 21:21:14 +0200

    Fix for sensor fusion again again

--- lib/mpu6050/mpu6050.h
+++ lib/mpu6050/mpu6050.h
@@ -19,5 +19,6 @@ uint8_t init_mpu6050(I2C_HandleTypeDef *i2c_address_temp, uint8_t apply_calibrat
 void mpu6050_get_accelerometer_readings_gravity(float *data);
 void mpu6050_get_gyro_readings_dps(float *data);
 void calculate_pitch_and_roll(float *data, float *roll, float *pitch);
+void calculate_degrees_x_y(float *data, float *rotation_around_x, float *rotation_around_y);
 void find_accelerometer_error(uint64_t sample_size);
 void find_gyro_error(uint64_t sample_size);
