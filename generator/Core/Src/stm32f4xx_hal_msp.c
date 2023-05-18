0ca1789f09b2f7c15800ca7d5121e83c0866d90b
Rokas <50622827+Rokasbarasa1@users.noreply.github.com>
2023-05-18 17:08:42 +0200

    Changes to pwm frequency

--- generator/Core/Src/stm32f4xx_hal_msp.c
+++ generator/Core/Src/stm32f4xx_hal_msp.c
@@ -267,7 +267,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_11;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
@@ -289,7 +289,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
---
+++ timer.py
@@ -0,0 +1,23 @@
+# helper script for making pwm calculations
+
+timer_peripheral_clock = 50000000
+pre_scaler = 250
+counter_period = 4000
+
+print("Timer peripheral clock: " +str(timer_peripheral_clock) + "Hz")
+print("Pre scaler value: " +str(pre_scaler))
+print("Counter period: " +str(counter_period))
+
+
+timer_clock = timer_peripheral_clock /pre_scaler
+print("Timer clock divided by pre scaler: " +str(timer_clock) + "Hz")
+
+frequency = timer_clock/counter_period
+print("Frequency: " + str(frequency) + "Hz")
+print("Max duty cycle: " + str(1000/ frequency) + "ms")
+print("Pwm value range: 0-" + str(counter_period))
+print("Resolution per pwm step: " + str((1000 / frequency)/counter_period) +"ms")
+
+#$used = 50
+#duty_cycle = used / counter_period
+
